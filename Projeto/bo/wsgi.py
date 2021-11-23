1b143be3d4ca3c4ef341cef8c01b149dbd374be8
carvalhogui <carvalhoguilherme.1391@gmail.com>
2021-11-23 09:49:35 -0300

    No ramo main  Your branch is up to date with 'origin/main'.
     Mudanças a serem submetidas:
    	modified:   bairros/__pycache__/apps.cpython-38.pyc
    	modified:   bo/__pycache__/settings.cpython-38.pyc
    	modified:   bo/__pycache__/wsgi.cpython-38.pyc
    	modified:   bo/wsgi.py
    	modified:   manage.py

--- Projeto/bo/wsgi.py
+++ Projeto/bo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bo.settings.base')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bo.settings')
 
 application = get_wsgi_application()
